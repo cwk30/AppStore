6c9f4c62a75c5f543d255ca08ac53c7b48fef395
orcasteps <91866256+orcasteps@users.noreply.github.com>
2022-04-08 18:50:34 +0800

    11pm submit !!

--- AppStore/urls.py
+++ AppStore/urls.py
@@ -41,9 +41,9 @@ urlpatterns = [
     path('parent_profile', app.views.parent_profile, name='Parent Profile Page'),
     path('parent_bookings', app.views.parent_bookings, name='Parent Bookings'),
     path('view_applicants', app.views.view_applicants, name='View Applicants'),
-    path('nanny_profile_update', app.views.nanny_profile_update, name='Nanny Profile Update'),
-    path('nanny_availability_update', app.views.nanny_availability_update, name='Nanny Availability Update'),
-    path('parent_profile_update', app.views.parent_profile_update, name='Parent Profile Update'),
+    #path('nanny_profile_update', app.views.nanny_profile_update, name='Nanny Profile Update'),
+    #path('nanny_availability_update', app.views.nanny_availability_update, name='Nanny Availability Update'),
+    #path('parent_profile_update', app.views.parent_profile_update, name='Parent Profile Update'),
     path('elements', app.views.elements, name='Elements'),
     path('index', app.views.index, name='Index 1'),
     path('index2', app.views.index2, name='Index 2'),
@@ -51,8 +51,8 @@ urlpatterns = [
     path('nannyscheduleadd', app.views.nannyscheduleadd, name='nannyscheduleadd'),
     path('nannyscheduleview', app.views.nannyscheduleview, name='nannyscheduleview'),
     path('jobview/<str:id>', app.views.jobview, name='jobview'),
-    path('nannybrowsejobs', app.views.nannybrowsejobs, name='nannybrowsejobs'),
-    path('parentsbrowsenannies', app.views.parentsbrowsenannies, name='parentsbrowsenannies'),
+    #path('nannybrowsejobs', app.views.nannybrowsejobs, name='nannybrowsejobs'),
+    #path('parentsbrowsenannies', app.views.parentsbrowsenannies, name='parentsbrowsenannies'),
     path('nannyedit', app.views.nannyedit, name='nannyedit')
 
 ]
